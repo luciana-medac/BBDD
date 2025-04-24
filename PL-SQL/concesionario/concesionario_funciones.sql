39d3329d0b4108ac6d9dc4bd1522c69dca8673fd
lachthachi <lmg0043@alu.medac.es>
2025-04-24 11:24:39 +0200

    ejercicio 3 funciones

--- PL-SQL/concesionario/concesionario_funciones.sql
+++ PL-SQL/concesionario/concesionario_funciones.sql
@@ -17,6 +17,7 @@ END;
 
 /* PARA LLAMAR A LA FUNCION */
 SELECT * FROM vende;
+/
 DECLARE
     v_dni vende.dni_empleado%TYPE := &dni;
     v_suma_precio vende.precio%TYPE; -- para guardar la variable que nos retorna la funcion
@@ -51,6 +52,40 @@ BEGIN
     dbms_output.put_line('total de ventas: ' || v_total || ' del empleado con DNI ' || v_dni);
 END;
 
+/* EJERCICIO 3: Obtener el nombre de un cliente por su DNI (en caso de NO_DATA_FOUND lanzar excepción) */
+CREATE OR REPLACE FUNCTION nombre_cliente (v_dni cliente.dni%TYPE) 
+RETURN VARCHAR2
+    AS
+        v_nombre cliente.nombre%TYPE;
+BEGIN
+    SELECT
+        nombre
+        INTO v_nombre
+        FROM cliente
+        WHERE dni = v_dni;
+    RETURN v_nombre;
+END;
+
+/*EJECUTARLO*/
+
+DECLARE
+    v_nombre cliente.nombre%TYPE;
+    v_dni cliente.dni%TYPE := &dni;
+
+BEGIN
+    
+    v_nombre := nombre_cliente(v_dni);
+    
+EXCEPTION
+    WHEN no_data_found THEN
+        dbms_output.put_line('No existe el cliente');
+    WHEN OTHERS THEN
+        dbms_output.put_line('nombre: ' || v_nombre 
+                                        || ' del cliente con DNI: ' 
+                                        || v_dni);
+END;
+
+SELECT * FROM cliente;
 
 
 
