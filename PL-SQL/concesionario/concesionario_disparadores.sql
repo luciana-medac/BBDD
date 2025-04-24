0bcad10def2b108474ed7053f5892d0914379925
lachthachi <lmg0043@alu.medac.es>
2025-04-24 11:03:17 +0200

    ejercicio 3 disparadores

--- PL-SQL/concesionario/concesionario_disparadores.sql
+++ PL-SQL/concesionario/concesionario_disparadores.sql
@@ -1,4 +1,6 @@
 /* DISPARADORES O TRIGGERS */
+/*EJERCICIO 2: Crear un trigger que verifique el precio de compra de un coche antes de insertarlo en la tabla coche. 
+Si el precio es nulo o cero, el trigger debe evitar la inserción y lanzar una excepción. */
 CREATE OR REPLACE TRIGGER checkPrecioCompra 
 BEFORE INSERT ON coche -- antes de un insert en la tabla coche
 for each row -- por cada fila 
@@ -10,3 +12,20 @@ BEGIN
         END IF;
 
 END;
+
+ALTER TABLE coche ADD fecha_insercion DATE;
+
+/* EJERCICIO 3: Crear un trigger que automáticamente inserte la fecha actual en la columna fecha_insercion cada 
+vez que se añade un nuevo registro en la tabla coche.
+Hay que añadir antes un campo fecha_insercion en la tabla coche */
+CREATE OR REPLACE TRIGGER checkFechaActual
+BEFORE INSERT ON coche
+for each row
+
+BEGIN
+    :NEW.fecha_insercion := SYSDATE;
+END;
+
+
+
+
