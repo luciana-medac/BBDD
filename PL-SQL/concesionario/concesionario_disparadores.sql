8154fc663f153ad9d1a7238bea299b30f78dfbd0
lachthachi <lmg0043@alu.medac.es>
2025-04-30 14:13:00 +0200

    ejercicio 4: disparadores

--- PL-SQL/concesionario/concesionario_disparadores.sql
+++ PL-SQL/concesionario/concesionario_disparadores.sql
@@ -1,4 +1,16 @@
 /* DISPARADORES O TRIGGERS */
+
+/* EJERCICIO 1: TRIGGER QUE IMPIDE REGISTRAR UNA VENTA SI EL PRECIO ES MENOR QUE 1000  */
+CREATE OR REPLACE TRIGGER checkVenta
+BEFORE INSERT ON vende
+for each row
+    BEGIN
+        IF :NEW.precio < 1000
+        THEN 
+            RAISE_APPLICATION_ERROR(-20001, 'No se puede registrar un precio menor a 1000');
+        END IF;
+END;
+
 /*EJERCICIO 2: Crear un trigger que verifique el precio de compra de un coche antes de insertarlo en la tabla coche. 
 Si el precio es nulo o cero, el trigger debe evitar la inserción y lanzar una excepción. */
 CREATE OR REPLACE TRIGGER checkPrecioCompra 
@@ -26,15 +38,19 @@ BEGIN
     :NEW.fecha_insercion := SYSDATE;
 END;
 
-/* EJERCICIO 1: TRIGGER QUE IMPIDE REGISTRAR UNA VENTA SI EL PRECIO ES MENOR QUE 1000  */
-CREATE OR REPLACE TRIGGER checkVenta
-BEFORE INSERT ON vende
+/*EJERCICIO 4: CREAR UN TRIGGER QUE, ANTES DE ACTUALIZAR UN COCHE, REVISE SI EL CAMPO PRECIO_COMPRA HA CAMBIADO
+Y SI ES ASÍ, LE APLIQUE UN 21% DE IVA AUTOMÁTICAMENTE, SIEMPRE QUE EL NUEVO VALOR NO SEA NULL*/
+
+CREATE OR REPLACE TRIGGER checkIva
+BEFORE UPDATE OF precio_compra ON coche
 for each row
     BEGIN
-        IF :NEW.precio < 1000
-        THEN 
-            RAISE_APPLICATION_ERROR(-20001, 'No se puede registrar un precio menor a 1000');
+        IF :NEW.precio_compra IS NOT NULL 
+        THEN :NEW.precio_compra:= :NEW.precio_compra*1.21;
         END IF;
 END;
 
 
+UPDATE coche SET precio_compra = 12000 WHERE matricula = '4446GCN';
+
+SELECT * FROM coche;
